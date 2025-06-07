92500ac7e7310895a4b6a8db72cdb8678262a6a2
Kevin <kevinisking1996@gmail.com>
2025-06-08 00:05:22 +0800

    加入定時推播功能

--- index.js
+++ index.js
@@ -15,12 +15,12 @@ const USER_ID = 'U6169d7be03e2f9a1dc41a70f8a7f4fb5'; // 用戶 LINE ID
 
 // 定義要發送的訊息內容
 const dailyReminders = [
-  { time: '08:00', message: '早安☀️ Jarvis 問你：早餐吃了什麼呀？' },
-  { time: '10:00', message: '🍽 該紀錄早餐囉，吃了什麼呢？' },
-  { time: '13:00', message: '🥗 午餐時間到，吃了什麼呢？' },
-  { time: '19:00', message: '🌙 晚餐通常在這時候吃～有特別吃什麼嗎？' },
-  { time: '21:00', message: '🍱 晚餐記得紀錄，Jarvis 等你回報唷' },
-  { time: '20:00', message: '🏃‍♂️ Jarvis 提醒：今天運動安排記得完成唷！' },
+  { time: '08:00', message: '☀️ 早安，王子殿下👑，今天是充滿活力的一天💪🏻' },
+  { time: '10:00', message: '🥐 精神如何，早餐吃了什麼呢？' },
+  { time: '13:00', message: '🍱 休息啦，午餐吃了什麼呢？' },
+  { time: '19:00', message: '🍴 王子殿下👑，晚餐想吃什麼呢，快到ChatGPT跟我討論吧？' },
+  { time: '20:00', message: '🏃‍♂️ 吃飽休息後，記得該運動一下吧！' },
+  { time: '21:00', message: '🌙 最後....晚餐吃了什麼呢？' },
   { time: '23:00', message: '🛌 睡前提醒：今天有運動嗎？也讓我幫你整理三餐紀錄吧！' }
 ];
 
